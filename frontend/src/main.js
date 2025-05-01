3952d27120e38c956a031009943b5b966b06b9a5
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-04-30 21:35:05 -0400

    JSON populate to HTML Page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,31 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+
+
+
+
+const userData = {
+  firstName: "Alice",
+  lastName: "Johnson",
+  age: 28,
+  favoriteMeals: [
+    "Sushi Roll",
+    "Grilled Cheese Sandwich",
+    "Beef Tacos"
+  ],
+  contact: {
+    email: "alice@example.com",
+    phone: "555-1234"
+  }
+};
+
+const stringifiedUserData= JSON.stringify(userData);
+console.log(`Stringified JSON: ${stringifiedUserData}`);
+
+const parsedUserData = JSON.parse(stringifiedUserData);
+console.log ((parsedUserData));
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
@@ -18,7 +43,32 @@ document.querySelector('#app').innerHTML = `
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
+
+    <hr />
+    <h2 id="fullName"></h2>
+    <h3 id="age"></h3>
+    <address>
+      <div id="email"></div>
+      <div id="phone"></div>
+    </address>
+    <h4>Favorite Meals:</h4>
+    <ul id="list"></ul>
   </div>
 `
 
 setupCounter(document.querySelector('#counter'))
+
+document.getElementById('fullName').textContent = `FullName: ${userData.firstName} ${userData.lastName}`;
+document.getElementById('age').textContent = `Age: ${userData.age}`;
+document.getElementById('email').textContent = `Email: ${userData.contact.email}`;
+document.getElementById('phone').textContent = `Phone: ${userData.contact.phone}`;
+
+
+
+
+const favMeals = document.getElementById('list');
+userData.favoriteMeals.forEach(meal => {
+  const li = document.createElement('li');
+  li.textContent = meal;
+  favMeals.appendChild(li);
+});
--- frontend/src/script.js
+++ frontend/src/script.js
@@ -18,3 +18,8 @@ console.log(`Stringified JSON: ${stringifiedUserData}`);
 
 const parsedUserData = JSON.parse(stringifiedUserData);
 console.log ((parsedUserData));
+
+    document.getElementById('fullName').textContent = `${userData.firstName} ${userData.lastName}`;
+    document.getElementById('age').textContent = `Age: ${userData.age}`;
+    document.getElementById('email').textContent = `Email: ${userData.contact.email}`;
+    document.getElementById('phone').textContent = `Phone: ${userData.contact.phone}`;
