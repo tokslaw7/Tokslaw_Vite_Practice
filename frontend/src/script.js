c6696ec7ef2ab685c25f09521990cec4f5ea31d1
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-04-30 20:57:40 -0400

    Stringified Data

---
+++ frontend/src/script.js
@@ -0,0 +1,17 @@
+const userData = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    age: 28,
+    favoriteMeals: [
+      "Sushi Roll",
+      "Grilled Cheese Sandwich",
+      "Beef Tacos"
+    ],
+    contact: {
+      email: "alice@example.com",
+      phone: "555-1234"
+    }
+};
+
+const stringifiedUserData= JSON.stringify(userData);
+console.log(`Stringified JSON: ${stringifiedUserData}`);
